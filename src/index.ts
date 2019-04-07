9e4521be01ad70aad21e407e33652a81def8de4e
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-04-07 13:38:36 +0300

    Add three.js

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import './style.css';
 
 
 async function main() {
-  console.log('Hello world');
   const SHOULD_LOAD_LAZY_SCRIPT = true;
 
   if (SHOULD_LOAD_LAZY_SCRIPT) {
-    const lazyModule: any = await import(/* webpackChunkName: "lazy-script" */ './lazy-script');
-    console.log(lazyModule.default(1, 5));
+    await import(/* webpackChunkName: "lazy-script" */ './lazy-script');
+    console.log('executed');
   }
 }
 
--- src/lazy-script.ts
+++ src/lazy-script.ts
@@ -1,5 +1,40 @@
-console.log('Hello from lazy-script.ts!');
+import * as THREE from 'three';
 
-export default function sum(a: number, b: number) {
-  return a + b;
-};
+// Fix canvas size
+const canvas = document.getElementsByTagName('canvas')[0];
+const { offsetWidth: width, offsetHeight: height } = canvas;
+canvas.width = width;
+canvas.height = height;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+
+// Set-up scene
+// Ahahaha: https://github.com/mrdoob/three.js/pull/5835
+const _consoleLog = console.log;
+console.log = () => {};
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: window.devicePixelRatio == 1
+});
+console.log = _consoleLog;
+
+renderer.setSize(width, height);
+renderer.setClearColor(0xffffff, 1);
+camera.position.z = 5;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xfffff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate() {
+	requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
