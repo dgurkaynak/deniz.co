93a75a8f34096b440245e9c926b3c95a839f4694
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-04-07 18:03:32 +0300

    Append hash to webpack output files and clean-up dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'source-map',
   output: {
-    filename: '[name].js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -39,6 +40,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.css'
     }),
