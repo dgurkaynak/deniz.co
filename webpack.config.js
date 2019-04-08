b6388db6906ea65c53bcd0be9fcc2c95a2d959d8
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-04-08 15:21:05 +0300

    Custom web font

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [ 'file-loader' ]
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [ 'file-loader' ]
+       }
     ]
   },
   resolve: {
