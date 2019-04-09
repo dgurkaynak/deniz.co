42a09c6c13c0942e6dfb3a4484ec34a417cc7051
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-04-09 20:12:44 +0300

    Image resize utility function

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,62 @@ export function readImageData(image: HTMLImageElement, offsetX = 0, offsetY = 0,
   context.drawImage(image, 0, 0);
   return context.getImageData(offsetX, offsetY, width, height);
 }
+
+
+let resizeImageDataCanvas: HTMLCanvasElement;
+export async function resizeImage(image: HTMLImageElement, width?: number, height?: number): Promise<HTMLImageElement> {
+  if (!resizeImageDataCanvas) resizeImageDataCanvas = document.createElement('canvas');
+  const context = resizeImageDataCanvas.getContext('2d');
+  width = width || image.width;
+  height = height || image.height;
+  resizeImageDataCanvas.width = width;
+  resizeImageDataCanvas.height = height;
+  context.clearRect(0, 0, width, height);
+  context.drawImage(image, 0, 0, width, height);
+
+  const newImage = new Image();
+  const url = await canvasToURL(resizeImageDataCanvas);
+
+  return new Promise((resolve, reject) => {
+    newImage.onload = () => resolve(newImage);
+    newImage.onerror = reject;
+    newImage.src = url;
+  });
+}
+
+
+export async function toBlobAsync(canvas: HTMLCanvasElement, ...args: any[]): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    // Polyfill
+    if (!canvas.toBlob) {
+      const dataString = canvas.toDataURL('image/png');
+      resolve(dataURIToBlob(dataString));
+      return;
+    }
+
+    try {
+      canvas.toBlob(resolve, ...args);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+
+export async function canvasToURL(canvas: HTMLCanvasElement) {
+  const blob = await toBlobAsync(canvas);
+  return URL.createObjectURL(blob);
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
+export function dataURIToBlob(dataURI: string) {
+  const binStr = atob(dataURI.split(',')[1]);
+  const arr = new Uint8Array(binStr.length);
+
+  for (let i = 0; i < binStr.length; i++) {
+    arr[i] = binStr.charCodeAt(i);
+  }
+
+  return new Blob([arr]);
+}
