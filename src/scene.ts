226a126273d2076aa9817a0c386046c26f8bbe40
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-05-13 23:51:30 +0300

    Display single swap result with rotation change on mouse move

--- src/scene.ts
+++ src/scene.ts
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
 import debounce from 'lodash/debounce';
 import shuffle from 'lodash/shuffle';
-import SceneCard from './scene-card';
-import SceneCardManager from './scene-card-manager';
 import { swaps } from './assets/swaps/index';
 
+
+// Constants
+const PLANE_SEGMENT_COUNT = [ 40, 30 ];
 
 // Set-up the canvas
 const canvasContainer = document.getElementById('canvas-container');
@@ -36,17 +37,81 @@ renderer.setClearColor(0xffffff, 0);
 console.log = _consoleLog;
 
 // Set-up scene
-const cards: SceneCard[] = shuffle(swaps).map((swap) => {
-  return new SceneCard(null, () => swap.load());
-});
-const cardManager = new SceneCardManager({
-  camera,
-  cards,
-  scene,
-  screenWidth: width,
-  screenHeight: height
-});
-cardManager.init().then(() => console.log('manager loaded'));
+const geometry = new THREE.PlaneGeometry(1, 1, PLANE_SEGMENT_COUNT[0], PLANE_SEGMENT_COUNT[1]);
+let image: {
+  texture: THREE.Texture,
+  material: THREE.MeshBasicMaterial,
+  mesh: THREE.Mesh
+};
+let overlayImage: {
+  texture: THREE.Texture,
+  material: THREE.MeshBasicMaterial,
+  mesh: THREE.Mesh
+};
+
+
+/**
+ * Main function
+ */
+async function main() {
+  const swap = shuffle(swaps)[0];
+  const swapResult = await swap.load();
+
+
+  if (!image) {
+    const texture = new THREE.Texture(swapResult.image);
+    texture.needsUpdate = true;
+    const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    image = { texture, material, mesh };
+  } else {
+    const texture = new THREE.Texture(swapResult.image);
+    texture.needsUpdate = true;
+    image.material.map = texture;
+    image.texture.dispose();
+    image.texture = texture;
+  }
+
+  if (!overlayImage) {
+    const texture = new THREE.Texture(swapResult.overlayImage);
+    texture.needsUpdate = true;
+    const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    overlayImage = { texture, material, mesh };
+  } else {
+    const texture = new THREE.Texture(swapResult.overlayImage);
+    texture.needsUpdate = true;
+    overlayImage.material.map = texture;
+    overlayImage.texture.dispose();
+    overlayImage.texture = texture;
+  }
+
+  const viewport = fitPlaneToScreen(camera.position.z - 3, camera.fov, width / height);
+  const aspectRatio = swapResult.originalWidth / swapResult.originalHeight;
+
+  const cardScale = Math.min(
+    viewport.width,
+    viewport.height * aspectRatio
+  ); // actually width (because card size is 1x1)
+  const cardScaleY = cardScale / aspectRatio; // actually height (because card size is 1x1)
+
+  // Offset self-half
+  // y -= cardScaleY / 2;
+
+  [ image.mesh, overlayImage.mesh ].forEach((m) => {
+    // m.rotation.y = 30 * Math.PI / 180;
+    m.scale.setX(cardScale);
+    m.scale.setY(cardScaleY);
+    // m.position.setY(0);
+  });
+
+  (window as any).imageMesh = image.mesh;
+}
+
 
 
 /**
@@ -58,14 +123,26 @@ function animate() {
   rAFId = requestAnimationFrame(animate);
   frameCount++;
 
-  // Dummy scroll
-  camera.position.y -= 0.001; // TODO: Viewport'a gore scale olmali
-  if (frameCount % 30 == 0) {
-    cardManager.update();
-  }
-
   renderer.render(scene, camera);
 }
+
+
+/**
+ * Listen mouse move event
+ * TODO: Touch events
+ */
+const onMouseMove = debounce((e: MouseEvent) => {
+  if (!image || !overlayImage) return;
+  const { clientX: x, clientY: y } = e;
+  const { innerWidth: width, innerHeight: height } = window;
+  const rotationX = ((y / height) - 0.5) * (15 * Math.PI / 180);
+  const rotationY = ((x / width) - 0.5) * (30 * Math.PI / 180);
+  [ image.mesh, overlayImage.mesh ].forEach((m) => {
+    m.rotation.x = rotationX;
+    m.rotation.y = rotationY;
+  });
+}, 5);
+document.body.addEventListener('mousemove', onMouseMove, false);
 
 
 /**
@@ -99,6 +176,22 @@ function dispose() {
 
 
 /**
+ * Fit a plane to screen (snapping to sides)
+ */
+function fitPlaneToScreen(distance: number, cameraFov: number, screenAspectRatio: number) {
+  const vFov = cameraFov * Math.PI / 180;
+  const planeHeightAtDistance = 2 * Math.tan(vFov / 2) * distance;
+  const planeWidthAtDistance = planeHeightAtDistance * screenAspectRatio;
+  return {
+    width: planeWidthAtDistance,
+    height: planeHeightAtDistance
+  };
+}
+
+
+/**
  * Go go go
  */
-animate();
+main()
+  .then(() => animate())
+  .catch((err) => console.error(err));
