19bd436b1bbab6a1ff6ffb0f05a973abdc3fc58a
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-06-18 21:53:11 +0300

    Add about button if js enabled, and collapse/uncollapse on click

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 import './style.css';
 
 
+// Add javascript-enabled class
+document.body.classList.add('javascript-enabled');
+
+// Get some element references
+const mainElement = document.getElementById('main');
+const headingTextElement = document.getElementById('heading-text');
+
+// Add about button
+const aboutButtonElement = document.createElement('span');
+aboutButtonElement.id = 'about-button';
+aboutButtonElement.textContent = 'About';
+mainElement.insertBefore(aboutButtonElement, headingTextElement.nextSibling);
+
+// About button click
+aboutButtonElement.addEventListener('click', () => {
+  const isOpened = mainElement.classList.contains('opened');
+  if (isOpened) {
+    mainElement.classList.remove('opened');
+  } else {
+    mainElement.classList.add('opened');
+  }
+}, false);
+
+
 async function main() {
   // TODO: Maybe check webgl support and fallback?
   // TODO: Minimal loading effect maybe?
