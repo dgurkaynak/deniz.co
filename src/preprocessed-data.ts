3f05a076177794b2ede8c3de7ca3dfe3bbf14788
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-08-31 21:33:50 +0300

    Update pre-processed image texts

--- src/preprocessed-data.ts
+++ src/preprocessed-data.ts
@@ -6,12 +6,17 @@ const images = [
   {
     id: 'abraham-lincoln',
     resolutions: [512, 1024, 1536],
-    headingText: `Give me six hours to chop down a tree and I will spend the first four sharpening the axe. — Abraham Lincoln`
+    headingText: `"Give me six hours to chop down a tree and I will spend the first four sharpening the axe." — Abraham Lincoln`
   },
   {
     id: 'acdc',
     resolutions: [512, 1024, 1536],
-    headingText: `He believes in simplicity, like AC/DC.`
+    headingText: `Critics like to say every AC/DC record sounds the same, but he doesn't agree.`
+  },
+  {
+    id: 'adam',
+    resolutions: [512, 1024],
+    headingText: `He likes people-watching, or rather observing complex-systems.`
   },
   {
     id: 'apollo11',
@@ -21,7 +26,7 @@ const images = [
   {
     id: 'claude-shannon',
     resolutions: [512, 1024],
-    headingText: 'His favorite genious: Claude Shannon'
+    headingText: 'He thinks that Claude Shannon is the most under-appreciated genius ever.'
   },
   {
     id: 'cosmos',
@@ -31,52 +36,67 @@ const images = [
   {
     id: 'david-hockney',
     resolutions: [512, 1024, 1536],
-    headingText: 'Anything simple always interests me. — David Hockney'
+    headingText: '"Anything simple always interests me." — David Hockney'
+  },
+  {
+    id: 'deniz-baby-steps',
+    resolutions: [512, 1024, 1536],
+    headingText: 'He learns by failing, grows with baby-step iterations.'
+  },
+  {
+    id: 'deniz-first-pc',
+    resolutions: [512, 1024, 1536],
+    headingText: 'He got his first PC when he was six. It was IBM 486 and blew his mind.'
   },
   {
     id: 'friends',
     resolutions: [512, 1024, 1536],
-    headingText: 'He thinks Monica > Rachel > Phoebe.'
+    headingText: 'He thinks that Monica > Rachel > Phoebe.'
   },
   {
     id: 'gnr',
     resolutions: [512, 1024, 1536],
-    headingText: `TODO`
+    headingText: `He and his friends have a WhatsApp group named "Rockstars", but none of them are even close.`
   },
   {
     id: 'grace-hopper',
     resolutions: [512, 1024, 1536],
-    headingText: `TODO`
+    headingText: `He has a clock on his wall running counter-clockwise, reminding him that humans are allergic to change.`
   },
   {
     id: 'john-carmack',
     resolutions: [512, 1024],
-    headingText: `The speed of light sucks. - John Carmack`
+    headingText: `"The speed of light sucks." — John Carmack`
   },
   {
     id: 'lena',
     resolutions: [512, 1024],
-    headingText: `TODO`
+    headingText: `He has a copy of November 1972 issue of Playboy.`
   },
   {
     id: 'matrix2',
     resolutions: [512, 1024, 1536],
-    headingText: `He should have choosen blue pill.`
+    headingText: `Neo should have chosen the blue pill.`
   },
   {
     id: 'paul-leary',
     resolutions: [512, 1024],
-    headingText: `That’s what’s cool about working with computers. They don’t argue, they remember everything and they don’t drink all your beer. - Paul Leary`
+    headingText: `"That’s what’s cool about working with computers. They don’t argue, they remember everything and they don’t drink all your beer." — Paul Leary`
   },
   {
     id: 'seinfeld',
     resolutions: [512, 1024, 1536],
-    headingText: 'He loves watching Seinfeld while eating.'
+    headingText: 'He deeply appreciates nothingness. One of his favorite tv shows is about nothing. Because nothing is a prerequisite for something.'
+  },
+  {
+    id: 'tia-toomey',
+    resolutions: [512, 1024, 1536, 2048],
+    headingText: `He likes pushing himself to his limits, at least in a CrossFit box.`
   },
   {
     id: 'zeki-muren',
     resolutions: [512, 1024, 1536, 2048],
-    headingText: 'Gitme sana muhtacım - Zeki Müren'
+    headingText: `He feels that he's (emotionally) old enough to love Zeki Müren, one of the pillars of Turkish raki culture.`
   },
 ];
 
